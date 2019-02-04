565b284a981af2bedf9958276680f40c16ccbb00
Olivier Giniaux <oginiaux@gmail.com>
2019-02-04 01:10:37 +0100

    First commit

---
+++ LabelsForWindows/LabelsForWindows.cs
@@ -0,0 +1,35 @@
+﻿using SharpShell.Interop;
+using SharpShell.SharpIconOverlayHandler;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace LabelsForWindows
+{
+    public class LabelsForWindows : SharpIconOverlayHandler {
+
+        protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes) {
+            return true;
+            //try {
+            //    //  Get the file attributes.
+            //    var fileAttributes = new FileInfo(path);
+
+            //    //  Return true if the file is read only, meaning we'll show the overlay.
+            //    return fileAttributes.IsReadOnly;
+            //}
+            //catch (Exception) {
+            //    return false;
+            //}
+        }
+
+        protected override System.Drawing.Icon GetOverlayIcon() {
+            //  Return the read only icon.
+            return Properties.Resources.Green;
+        }
+
+        protected override int GetPriority() {
+            //  The read only icon overlay is very low priority.
+            return 90;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Labels-For-Windows
 MacOS style labels for Windows !
+
+# WIP
+SharpShell is not working well on my current setup (if not working at all).
+However, interface for Overlay Icons is good.
+Two COM registrations must be done (manually), one for x32, one for x64.
+The x32 one is required even on x64 systems since some file browsers are running x32.
+Alone, the x32 alone won't work for x64 file browsers such as explorer.exe.
+COM attributes but also be set manually.
+## x32
+`C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe`
+## x64
+`C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe`
