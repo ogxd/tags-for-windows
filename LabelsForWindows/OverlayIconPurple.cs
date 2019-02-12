63dbe9814d0f2db66c1849a1fc1a932254a4c1f2
Olivier Giniaux <oginiaux@gmail.com>
2019-02-13 00:49:49 +0100

    Add overlay for remaining colors (yellow, purple and blue)

--- LabelsForWindows/OverlayIconPurple.cs
+++ LabelsForWindows/OverlayIconPurple.cs
@@ -7,20 +7,20 @@ using SharpShell.SharpIconOverlayHandler;
 namespace LabelsForWindows {
 
     [ComVisible(true)]
-    [RegistrationName("  LabelsForWindows_Green")]
-    [Guid("a259c04f-ffa8-310b-864c-fe602840399a")]
-    public class OverlayIconGreen : SharpIconOverlayHandler {
+    [RegistrationName("  LabelsForWindows_Purple")]
+    [Guid("a259c04f-ffa8-310b-864c-fe602840399d")]
+    public class OverlayIconPurple : SharpIconOverlayHandler {
 
         protected override int GetPriority() {
             return 90;
         }
 
         protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes) {
-            return Manager.GetIcon(path) == "green";
+            return Manager.GetIcon(path) == "purple";
         }
 
         protected override System.Drawing.Icon GetOverlayIcon() {
-            return Properties.Resources.Green;
+            return Properties.Resources.Purple;
         }
     }
 }
--- LabelsForWindows/OverlayIconYellow.cs
+++ LabelsForWindows/OverlayIconYellow.cs
@@ -7,20 +7,20 @@ using SharpShell.SharpIconOverlayHandler;
 namespace LabelsForWindows {
 
     [ComVisible(true)]
-    [RegistrationName("  LabelsForWindows_Green")]
-    [Guid("a259c04f-ffa8-310b-864c-fe602840399a")]
-    public class OverlayIconGreen : SharpIconOverlayHandler {
+    [RegistrationName("  LabelsForWindows_Yellow")]
+    [Guid("a259c04f-ffa8-310b-864c-fe602840399b")]
+    public class OverlayIconYellow : SharpIconOverlayHandler {
 
         protected override int GetPriority() {
             return 90;
         }
 
         protected override bool CanShowOverlay(string path, FILE_ATTRIBUTE attributes) {
-            return Manager.GetIcon(path) == "green";
+            return Manager.GetIcon(path) == "yellow";
         }
 
         protected override System.Drawing.Icon GetOverlayIcon() {
-            return Properties.Resources.Green;
+            return Properties.Resources.Yellow;
         }
     }
 }
