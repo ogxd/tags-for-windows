ad45e9e02290619f633f84244fe0663daab5e1d7
Olivier Giniaux <oginiaux@gmail.com>
2019-02-13 00:51:58 +0100

    Cleanup

--- LabelsForWindows/Extensions.cs
+++ LabelsForWindows/Extensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
 
 namespace LabelsForWindows {
 
@@ -21,6 +19,26 @@ namespace LabelsForWindows {
             target.CopyTo(result, 0);
             items.CopyTo(result, target.Length);
             return result;
+        }
+
+
+        private static float _Dpi = -1;
+        public static float Dpi {
+            get {
+                if (_Dpi == -1) {
+                    using (Graphics graphics = Graphics.FromHwnd(IntPtr.Zero)) {
+                        _Dpi = graphics.DpiY;
+                    }
+                }
+                return _Dpi;
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("Shell32.dll")]
+        private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
+
+        public static void RefreshExplorer() {
+            SHChangeNotify(0x8000000, 0x1000, IntPtr.Zero, IntPtr.Zero);
         }
     }
 }
--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,9 +1,5 @@
 ﻿using LabelsForWindows;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tests {
 
@@ -11,11 +7,14 @@ namespace Tests {
 
         static void Main(string[] args) {
 
-            Manager.AssignIcon("test1", "red");
-            Manager.AssignIcon("test2", "yellow");
-            Manager.AssignIcon("test1", "blue");
-            Manager.AssignIcon("test3", "purple");
-            Manager.UnassignIcon("test1");
+            //Manager.AssignIcon("test1", "red");
+            //Manager.AssignIcon("test2", "yellow");
+            //Manager.AssignIcon("test1", "blue");
+            //Manager.AssignIcon("test3", "purple");
+            //Manager.UnassignIcon("test1");
+
+            Console.WriteLine(ContextMenu.GetBitmap("Green"));
+
             Console.ReadKey();
         }
     }
