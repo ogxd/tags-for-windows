4f5b6a3a99da8e6aef80ac5315ce326071b2164a
Olivier Giniaux <olivier.giniaux@pi.xyz>
2020-09-23 17:27:40 +0200

    Add automatic versionning

--- TagsForWindows/Properties/AssemblyInfo.cs
+++ TagsForWindows/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReadOnlyFileIconOverlayHandler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Tags for Windows")]
+[assembly: AssemblyProduct("Tags for Windows")]
+[assembly: AssemblyDescription("Tag files and folders in Windows (Like in MacOS)")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReadOnlyFileIconOverlayHandler")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Ogxd")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b8ae651d-0143-420d-9b65-b356ab2c3d67")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
